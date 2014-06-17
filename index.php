08d4866f90dfe00365f54376864795210e5722ff
breconwhite <breconwhite@hotmail.com>
2014-06-17 14:25:18 -0400

    Initial uploade

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script type="text/javascript" src="particle.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+	</head>
+
+	<body>
+		<div class="logo"><img src="banner.jpg"/></div>
+		<div id="container">
+			<canvas id="pixie"></canvas>
+		</div>
+	</body>
+</html>
